b495dc53d1db146daeedd50814f048da25098af4
luanlsr <luanfswd@gmail.com>
2024-02-28 04:50:41 -0300

    Agora vai!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import { useState } from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <>
-      <h1>HOME</h1>
-    </>
-  )
-}
+    <div className="container mx-auto mt-10">
+      <h1 className="text-4xl font-bold mb-4">Bem-vindo ao NO-KCAL</h1>
+      <p className="text-lg mb-8">Esta é uma página inicial simples criada com Next.js, Tailwind CSS, Bootstrap, Nesj.js e MongoDb</p>
+    </div>
+  );
+};
+
+export default Home;
